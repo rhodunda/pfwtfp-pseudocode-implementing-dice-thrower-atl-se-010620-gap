28554ab90b63de902e5f1c723da4995cc916e8a9
maxwellbenton <maxwellbenton@gmail.com>
2018-11-02 12:04:23 -0400

    removed filler code

--- spec/pseudocode_spec.rb
+++ spec/pseudocode_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 PLACEHOLDER_STRING = "Replace this text with your response"
 
-describe 'Pseudocode provided in the HTML file answers descriptions of each of the components of the Flatiron Process' do
+describe 'Pseudocode Roll History' do
   before(:all) do
-    doc = File.open("pseudocode.html") { |f| Nokogiri::XML(f) }
+    doc = File.open("01_roll_history_pseudocode.html") { |f| Nokogiri::XML(f) }
     @lookup = doc.css("body > article[id]").each_with_object({}) do |node, memo|
       memo[node.attr("id")] = node.css("section > p.response").text.strip
     end
@@ -28,3 +28,54 @@ describe 'Pseudocode provided in the HTML file answers descriptions of each of t
   end
 end
 
+describe 'Pseudocode Special Roll Conditions' do
+  before(:all) do
+    doc = File.open("02_special_roll_conditions_pseudocode.html") { |f| Nokogiri::XML(f) }
+    @lookup = doc.css("body > article[id]").each_with_object({}) do |node, memo|
+      memo[node.attr("id")] = node.css("section > p.response").text.strip
+    end
+  end
+
+  it "Has a non-default value for 'identify_the_problem'" do
+    expect(@lookup['identify_the_problem']).to_not match(PLACEHOLDER_STRING)
+  end
+
+  it "Has a non-default value for 'identify_the_output'" do
+    expect(@lookup["identify_the_output"]).to_not match(PLACEHOLDER_STRING)
+  end
+  it "Has a non-default value for 'name_the_procedure'" do
+    expect(@lookup["name_the_procedure"]).to_not match(PLACEHOLDER_STRING)
+  end
+  it "Has a non-default value for 'identify_what_inputs'" do
+    expect(@lookup["identify_what_inputs"]).to_not match(PLACEHOLDER_STRING)
+  end
+  it "Has a non-default value for 'define_the_procedure'" do
+    expect(@lookup["define_the_procedure"]).to_not match(PLACEHOLDER_STRING)
+  end
+end
+
+describe 'Pseudocode Multisided Dice Rolls' do
+  before(:all) do
+    doc = File.open("03_multisided_dice_pseudocode.html") { |f| Nokogiri::XML(f) }
+    @lookup = doc.css("body > article[id]").each_with_object({}) do |node, memo|
+      memo[node.attr("id")] = node.css("section > p.response").text.strip
+    end
+  end
+
+  it "Has a non-default value for 'identify_the_problem'" do
+    expect(@lookup['identify_the_problem']).to_not match(PLACEHOLDER_STRING)
+  end
+
+  it "Has a non-default value for 'identify_the_output'" do
+    expect(@lookup["identify_the_output"]).to_not match(PLACEHOLDER_STRING)
+  end
+  it "Has a non-default value for 'name_the_procedure'" do
+    expect(@lookup["name_the_procedure"]).to_not match(PLACEHOLDER_STRING)
+  end
+  it "Has a non-default value for 'identify_what_inputs'" do
+    expect(@lookup["identify_what_inputs"]).to_not match(PLACEHOLDER_STRING)
+  end
+  it "Has a non-default value for 'define_the_procedure'" do
+    expect(@lookup["define_the_procedure"]).to_not match(PLACEHOLDER_STRING)
+  end
+end
